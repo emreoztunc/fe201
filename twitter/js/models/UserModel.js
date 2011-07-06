522d8e4de092c6a6d49e7e45aa13f9eea915ddc2
Emre Oztunc <emre.oztunc@gmail.com>
2011-07-06 16:15:55 +0300

    Creator comment changes

--- twitter/js/models/UserModel.js
+++ twitter/js/models/UserModel.js
@@ -1,13 +1,61 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Emre
+ * Date: 05.07.2011
+ * Time: 09:55
+ * To change this template use File | Settings | File Templates.
+ */
 
-var UserModel = function() {
+UserModel = function () {};
+
+UserModel.prototype.createUser = function (userName, password, eMail){
+    var user = new User (userName, password, eMail);
+    localStorage.setObject(user.id, user);
+    var phot = "fff";
+    var locat = "kkk";
+    var bio = "bio";
+
+    ProfileModel.createProfile(phot, locat, bio, user.userId);
+    //user.tweets = TweetModel.getTweetsByUserId(user.id);
 
 };
 
-UserModel.prototype.createUser = function(username, password, email) {
-    var user = new User(username, password, email);
-    localStorage.setObject(user.id, user);
-}
+UserModel.prototype.removeUser = function (userId) {
+    for (var user in localStorage) {
+	user = localStorage.getObject(user);
+	if (user.userId == userId) {
+        localStorage.removeItem(user)
+        }
+    }
+};
+
+UserModel.prototype.login = function (userName, password) {
+    for (var user in localStorage) {
+        if ((user.userName == userName) &&(user.password == password)) {
+            var session = new Session(user.id);
+            localStorage.push("Session", session);
+        }
+    }
+};
+
+UserModel.prototype.logout = function () {
+    localStorage.removeItem("Session");
+};
+
+UserModel.prototype.getUserById = function (userId) {
+    for (var user in localStorage) {
+	user = localStorage.getObject(user);
+	if (user.id == userId) {
+        return item;
+        }
+    }
+};
+
+UserModel.prototype.getUserByUsername = function (userName) {
+    for (var user in localStorage) {
+	user = localStorage.getObject(user);
+	if (user.userName == userName) {
+        return user;
+        }
+    }
+};
