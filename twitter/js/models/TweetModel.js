f303732002d72be770801886e948b68f993b09a4
Emre Oztunc <emre.oztunc@gmail.com>
2011-07-06 16:16:56 +0300

    TweetModel.js content updated.

--- twitter/js/models/TweetModel.js
+++ twitter/js/models/TweetModel.js
@@ -1,13 +1,35 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Emre
+ * Date: 05.07.2011
+ * Time: 09:55
+ * To change this template use File | Settings | File Templates.
+ */
 
-var TweetModel = function() {
+TweetModel = function () {};
 
+TweetModel.prototype.getTweetsByUserId = function (userId) {
+    var tweets = [{}];
+    for (var tweet in localStorage) {
+	tweet = localStorage.getObject(tweet);
+	if (tweet.userId == userId) {
+        tweets.push(tweet);
+        }
+    }
+    return tweets;
 };
 
-TweetModel.prototype.postTweet = function(user, body) {
-    var tweet = new Tweet(user, body);
+TweetModel.prototype.postTweet = function (user, body) {
+    var tweet = new Tweet (user, body);
     localStorage.setObject(tweet.id, tweet);
-}
+};
+
+TweetModel.prototype.removeTweet = function (tweetId) {
+    for (var tweet in localStorage) {
+	tweet = localStorage.getObject(tweet);
+	if (tweet.tweet.id == tweetId) {
+        localStorage.removeItem(item);
+        }
+    }
+};
+
