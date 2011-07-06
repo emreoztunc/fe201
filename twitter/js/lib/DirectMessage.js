d5c0fae82b957f83b16536c95c7cae25a15c336b
Emre Oztunc <emre.oztunc@gmail.com>
2011-07-06 16:13:16 +0300

    Changes in DirectMessage.js: Comments and constructor added

--- twitter/js/lib/DirectMessage.js
+++ twitter/js/lib/DirectMessage.js
@@ -1,4 +1,14 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Emre
+ * Date: 05.07.2011
+ * Time: 09:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+var DirectMessage = function (fromId, toId, body){
+    this.id = tart.getUId();
+    this.fromId = fromId;
+    this.toId = toId;
+    this.body = body;
+};
