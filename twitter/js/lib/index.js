c9589197a543970f06998d0c4513dd5a8d46db2c
Emre Oztunc <emre.oztunc@gmail.com>
2011-07-25 11:59:16 +0300

    Index.js will be swapped with index.html. This script will take the responsibility of index.html.

--- twitter/js/lib/index.js
+++ twitter/js/lib/index.js
@@ -5,3 +5,13 @@
  * Time: 20:35
  * To change this template use File | Settings | File Templates.
  */
+
+goog.require('tart.string');
+goog.require('tart.date');
+goog.provide('twitter.index');
+
+twitter.index = function() {
+
+}
+
+goog.exportSymbol('twitter.index', twitter.index);
