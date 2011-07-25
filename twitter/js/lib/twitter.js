fc0102d6bae61522a1a50b915f6afc44e889402e
Emre Oztunc <emre.oztunc@gmail.com>
2011-07-25 12:00:30 +0300

    This is the part of the conversion from "html" oriented structure to "js" oriented one.

--- twitter/js/lib/twitter.js
+++ twitter/js/lib/twitter.js
@@ -5,3 +5,10 @@
  * Time: 22:09
  * To change this template use File | Settings | File Templates.
  */
+
+goog.require('twitter.setup');
+goog.require('twitter.index');
+goog.require('tart.storage.Storage');
+goog.provide('twitter');
+
+twitter.localStorage = new tart.storage.Storage();
